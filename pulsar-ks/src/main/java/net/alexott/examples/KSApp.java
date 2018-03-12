d81cd0d5a284c0f02735f4a03371719a447a6134
Alex Ott <alex.ott@datastax.com>
2018-03-12 14:46:35 -0700

    found the problem of incompatibility

--- pulsar-ks/src/main/java/net/alexott/examples/KSApp.java
+++ pulsar-ks/src/main/java/net/alexott/examples/KSApp.java
@@ -7,13 +7,12 @@ import java.util.regex.Pattern;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
 
 public class KSApp {
 	public static void main(String[] args) {
@@ -24,22 +23,24 @@ public class KSApp {
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "pulsar://localhost:6650");
 		props.put("key.deserializer", StringDeserializer.class.getName());
 		props.put("value.deserializer", StringDeserializer.class.getName());
+		props.put("key.serializer", StringSerializer.class.getName());
+		props.put("value.serializer", StringSerializer.class.getName());		
 		props.put("group.id", "test1");
-		props.put("enable.auto.commit", "false");
+//		props.put("enable.auto.commit", "false");
 
-		StreamsBuilder builder = new StreamsBuilder();
-		KStream<String, String> textLines = builder.stream("persistent://sample/standalone/ns1/test1", Consumed.with(stringSerde, stringSerde));
+		KStreamBuilder builder = new KStreamBuilder();
+		KStream<String, String> textLines = builder.stream(stringSerde, stringSerde, "persistent://sample/standalone/ns1/test1");
 
 		final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
 
 		final KTable<String, String> wordCounts = textLines
 				.flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase()))).groupBy((key, word) -> word)
-				.count().mapValues(value -> Long.toString(value));
+				.count("test").mapValues(value -> Long.toString(value));
 
 		// Write the `KTable<String, Long>` to the output topic.
-		wordCounts.toStream().to("persistent://sample/standalone/ns1/test-wc", Produced.with(stringSerde, stringSerde));
+		wordCounts.toStream().to(stringSerde, stringSerde, "persistent://sample/standalone/ns1/test-wc");
 		
-		final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+		final KafkaStreams streams = new KafkaStreams(builder, props);
 		streams.cleanUp();
 		streams.start();
 		Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
